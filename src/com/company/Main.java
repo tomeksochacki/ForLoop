369639dc56b5b34430efdbe37352d8766ad89025
tomek <tomeksochacki@wp.pl>
2020-02-12 13:43:24 +0100

    Initialize repository

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int suma = 0;
+
+	for (int i = 0; i<100; i++){
+	    suma += i;
+
+	    }
+        System.out.println("Suma wynosi: " + suma);
+    }
+}
